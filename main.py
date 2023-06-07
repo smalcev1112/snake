c54e872fe20e1ce974636a19389dee5d51b267c3
smalcev1112 <113887255+smalcev1112@users.noreply.github.com>
2023-06-07 14:41:13 +0800

    Автопилот
    1. Score изменил на len.
    2. В строке вывода текста добавил вывод максимальной длины змейки за сессию.
    3. Змейка растет только тогда, когда кол-во съеденных яблок равняется ее длине.
    4. После увеличения размера змейки, кол-во съеденных яблок обнуляется.
    5. Для отрисовки мира и отображения длины змейки используются отдельные viewport-ы.
    6. Удалил draw_side_bar().
    7. Змейка меняет свое направление движения самостоятельно, если в поле ее зрения попадает яблоко или она сама.
    8. Поле зрения змейки - это три клетки типа Cell(слева, справа и впереди головы змейки)
    9. При управлении змейки с клавиатуры, автопилот змейки приостанавливается на время (примерно 3-5с., зависит от скорости движения змейки)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import pygame
 import random
 import sys
@@ -21,6 +23,8 @@ APPLE_COLOR = (255, 0, 0)
 APPLE_OUTER_COLOR = (155, 0, 0)
 SNAKE_COLOR = (0, 255, 0)
 SNAKE_OUTER_COLOR = (0, 155, 0)
+VISION_COLOR = (255, 170, 200)
+VISION_OUTER_COLOR = (230, 150, 180)
 HEAD_COLOR = (173, 255, 47)
 HEAD_OUTER_COLOR = (154, 205, 50)
 TEXT_COLOR = (255, 100, 0)
@@ -53,6 +57,9 @@ class Cell:
     def __ge__(self, other):
         return self.x >= other.x or self.y >= other.y
 
+    def __neg__(self):
+        return Cell(self.x * (-1), self.y * (-1))
+
     def __add__(self, other):
         return Cell(self.x + other.x, self.y + other.y)
 
@@ -70,22 +77,57 @@ class Cell:
         x = self.x * CELL_SIZE
         y = self.y * CELL_SIZE
         r = pygame.Rect(x, y, CELL_SIZE, CELL_SIZE)
-        pygame.draw.rect(DISPLAY, outer_color, r, 0)
+        pygame.draw.rect(viewport1, outer_color, r, 0)
         #  * меньший - inner_color.
         x += BORDER_SIZE
         y += BORDER_SIZE
         size = CELL_SIZE - BORDER_SIZE * 2
         r = pygame.Rect(x, y, size, size)
-        pygame.draw.rect(DISPLAY, inner_color, r, 0)
+        pygame.draw.rect(viewport1, inner_color, r, 0)
 
 
 class Snake:
     def __init__(self):
         self.cells = [Cell(10, i) for i in range(3, 6)]
         self.direction_of_head = self.cells[-1] - self.cells[-2]
+        self.vision = self.get_vision()
+
+    def get_direction_of_head(self):
+        d_last = self.direction_of_head
+        d = self.cells[-1] - self.cells[-2]
+        if -1 <= d.x <= 1 and -1 <= d.y <= 1:
+            return d
+        else:
+            return d_last
+
+    def get_vision(self):
+        cells = [Cell(-1, 0), Cell(0, -1), Cell(1, 0), Cell(0, 1)]
+        if -self.direction_of_head in cells:
+            cells.remove(-self.direction_of_head)
+        for cell in cells:
+            cell.x += self.cells[-1].x
+            cell.x %= WIDTH
+            cell.y += self.cells[-1].y
+            cell.y %= HEIGHT
+        return cells
+
+    def make_decision(self, apples):
+        possible_way = deepcopy(self.vision)
+        for eye in self.vision:
+            if see_apple(eye, apples):
+                return eye - self.cells[-1]
+            if see_self(eye, self.cells):
+                possible_way.remove(eye)
+        if len(possible_way) == 3 or len(possible_way) == 0:
+            if random.randrange(10) > 0:
+                return self.direction_of_head
+            else:
+                return random.sample(possible_way, 1)[0] - self.cells[-1]
+        if len(possible_way) < 3:
+            return random.sample(possible_way, 1)[0] - self.cells[-1]
 
     def move(self, direction):
-        # перемещение змейки на одну ячейку в заданном направлении.
+        """Функция перемещения змейки на одну ячейку в заданном направлении."""
         if direction != Cell(0, 0):
             self.cells.pop(0)
             new_head = self.cells[-1] + direction
@@ -93,37 +135,40 @@ class Snake:
             if EDGELESS:
                 new_head %= Cell(WIDTH, HEIGHT)
             self.cells.append(new_head)
-            self.direction_of_head = self.cells[-1] - self.cells[-2]
+            self.direction_of_head = self.get_direction_of_head()
 
     def draw_snake(self):
+        """Функция рисования змеи"""
         # голова змеи
         self.cells[-1].draw(SNAKE_OUTER_COLOR, HEAD_COLOR)
+        # область зрения
+        for item in self.vision:
+            item.draw(VISION_OUTER_COLOR, VISION_COLOR)
         # хвост змеи
-
         for item in self.cells[-2::-1]:
             item.draw(SNAKE_OUTER_COLOR, SNAKE_COLOR)
 
     def hit_edge(self):
-        # функция возвращает True,
-        # если змейка пересекла одну из границ окна.
+        """Функция возвращает True,
+        если змейка пересекла одну из границ окна."""
         if self.cells[-1] < Cell(0, 0) or self.cells[-1] > Cell(WIDTH, HEIGHT):
             return True
 
     def hit_apple(self, apples):
-        # функция возвращает True, если голова
-        # змейки находится в той же ячейке, что и яблоко.
+        """Функция возвращает True, если голова
+        змейки находится в той же ячейке, что и яблоко."""
         for apple in apples:
             if self.cells[-1] == apple:
                 apples.remove(apple)
                 return True
 
     def grow(self):
-        # увеличения длины змейки.
+        """Функция увеличения длины змейки."""
         self.cells.insert(0, self.cells[0])
 
     def bite_self(self):
-        # функция возвращает True, если голова змеи
-        # пересеклась хотя бы с одним блоком хвоста.
+        """Функция возвращает True, если голова змеи
+        пересеклась хотя бы с одним блоком хвоста."""
         for cell in self.cells[:-1]:
             if self.cells[-1] == cell:
                 return True, self.cells.index(cell)
@@ -132,7 +177,7 @@ class Snake:
 
 class Apple:
     def __init__(self):
-        self.cells = [add_apple(), ]
+        self.cells = [add_apple() for _ in range(10)]
 
     def draw_apple(self):
         for apple in self.cells:
@@ -142,12 +187,16 @@ class Apple:
 def main():
     global FPS_CLOCK
     global DISPLAY
+    global viewport1, viewport2
+    global font
 
     pygame.init()
     FPS_CLOCK = pygame.time.Clock()
     DISPLAY = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption('Wormy')
-
+    viewport1 = create_viewport(WINDOW_WIDTH-4*CELL_SIZE, WINDOW_HEIGHT)
+    viewport2 = create_viewport(4*CELL_SIZE, WINDOW_HEIGHT)
+    font = pygame.font.SysFont('couriernew', 12)
     while True:
         # Game... Game never changes.
         run_game()
@@ -156,15 +205,18 @@ def main():
 def run_game():
     apples = Apple()
     snake = Snake()
-    font = pygame.font.SysFont('couriernew', 12)
     # исходное направление движения змейки.
     direction = snake.direction_of_head
     speed = 3
+    count_apple = 0
+    max_len = len(snake.cells)
+    manual_control, count = False, 50
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
             if event.type == pygame.KEYDOWN:
+                manual_control = True
                 direction = get_direction(event, direction)
                 # смена скорости змейки
                 tmp = get_speed(event)
@@ -182,10 +234,13 @@ def run_game():
             snake.cells = snake.cells[index:]
         # Обработка ситуации столкновения змейки с яблоком.
         #  При столкновении:
-        #   * увеличить размер змейки
+        #   * увеличить размер змейки, если кол-во съеденных яблок равняется длине змейки
         #   * добавить новое яблоко
         if snake.hit_apple(apples.cells):
-            snake.grow()
+            count_apple += 1
+            if count_apple >= len(snake.cells):
+                count_apple = 0
+                snake.grow()
             if len(apples.cells) <= 10:
                 apples.cells.append(add_apple())
                 # Если яблоко на змее, то располагаем яблоко позади хвоста
@@ -193,46 +248,68 @@ def run_game():
                     if cell == apples.cells[-1]:
                         apples.cells[-1] = snake.cells[0] + (snake.cells[0] - snake.cells[1])
                         break
+        if not manual_control:
+            count = 20
+            direction = snake.make_decision(apples)
+        else:
+            count -= 1
+            if count <= 0:
+                manual_control = False
         # сдвинуть змейку в заданном направлении
         snake.move(direction)
-        text = font.render(f'Score:{len(snake.cells)}', True, TEXT_COLOR)
+        snake.vision = snake.get_vision()
+        if max_len < len(snake.cells):
+            max_len = len(snake.cells)
+        s = f'Len:{len(snake.cells)} ({max_len})'
+        text = font.render(s, True, TEXT_COLOR)
         draw_frame(snake, apples, text)
 
         FPS_CLOCK.tick(FPS * speed)
 
 
 def draw_frame(snake, apples, text):
-    DISPLAY.fill(BG_COLOR)
+    viewport1.fill(BG_COLOR)     # область мира змейки
+    viewport2.fill(GRID_COLOR)
+    viewport2.blit(text, (3, 5))
     draw_grid()
-    draw_side_bar()
-    DISPLAY.blit(text, (WINDOW_WIDTH - 3.7 * CELL_SIZE, 5))
     snake.draw_snake()
     apples.draw_apple()
+    DISPLAY.blit(viewport1, (0, 0))
+    DISPLAY.blit(viewport2, (WINDOW_WIDTH - 4 * CELL_SIZE, 0))
     pygame.display.update()
 
 
-def draw_side_bar():
-    r = pygame.Rect(WINDOW_WIDTH - 4 * CELL_SIZE, 0, 4 * CELL_SIZE, WINDOW_HEIGHT)
-    pygame.draw.rect(DISPLAY, GRID_COLOR, r, 0)
-
-
 def draw_grid():
-    for i in range(WIDTH + 1):
-        pygame.draw.line(DISPLAY, GRID_COLOR, (i * CELL_SIZE, 0), (i * CELL_SIZE, WINDOW_HEIGHT))
+    """Функция заполняет всю поверхность viewport, вертикальными и горизонтальными линиями с шагом CELL_SIZE пикселей"""
+    for i in range(WIDTH+1):
+        pygame.draw.line(viewport1, GRID_COLOR, (i * CELL_SIZE, 0), (i * CELL_SIZE, viewport1.get_size()[1]))
     for i in range(HEIGHT):
-        pygame.draw.line(DISPLAY, GRID_COLOR, (0, i * CELL_SIZE), (WINDOW_WIDTH - 4 * CELL_SIZE, i * CELL_SIZE))
+        pygame.draw.line(viewport1, GRID_COLOR, (0, i * CELL_SIZE), (viewport1.get_size()[0], i * CELL_SIZE))
 
 
 def add_apple():
-    # функция возвращает случайную ячейку,
-    # в которой будет располагаться яблоко.
+    """Функция возвращает ячейку со случайными координатами Cell(x, y)."""
     return Cell(random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1))
 
 
+def see_apple(eye, apples):
+    """Если змея увидела яблоко, то функция возвращает True"""
+    for apple in apples.cells:
+        if eye == apple:
+            return True
+
+
+def see_self(eye, cells):
+    """Если змея увидела себя, то функция возвращает True"""
+    for cell in cells[0:-4]:
+        if eye == cell:
+            return True
+
+
 def get_direction(event, direction):
-    # функция возвращает направление движения
-    #  Если нажата клавиша противоположного направления движения,
-    #  то не менять направление.
+    """Функция возвращает направление движения.
+    Если нажата клавиша противоположного направления движения,
+    то не менять направление."""
     if event.key == pygame.K_LEFT and direction.x != 1:
         return Cell(-1, 0)
     elif event.key == pygame.K_RIGHT and direction.x != -1:
@@ -247,7 +324,14 @@ def get_direction(event, direction):
         return direction
 
 
+def create_viewport(width, height):
+    """Функция создает поверхность(Surface) с заданными размерами (width, height)"""
+    surface = pygame.Surface((width, height))
+    return surface
+
+
 def get_speed(event):
+    """Функция возвращает число от 1 до 5, в зависимости от нажатия клавиш 1-5"""
     if event.key == pygame.K_1:
         return 1
     elif event.key == pygame.K_2:
